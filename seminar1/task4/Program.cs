d992c4f88bdc54d53128c5839b800aa1762ffc0a
Yulia Romanovich <cruciverba@yandex.ru>
2022-09-03 23:05:22 +0300

    realized task4

---
+++ seminar1/task4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB && numberA > numberC)
+{
+    Console.WriteLine("max: {0}", numberA);
+}
+else if(numberB > numberA && numberB > numberC)
+{
+    Console.WriteLine("max: {0}", numberB);
+}
+
+else if(numberC > numberA && numberC > numberB)
+{
+    Console.WriteLine("max: {0}", numberC);
+}
