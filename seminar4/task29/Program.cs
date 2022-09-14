fe30563949b07c2a86ca2fb3e8e37500142a3ff5
Yulia Romanovich <cruciverba@yandex.ru>
2022-09-14 11:01:21 +0300

    realized task29

---
+++ seminar4/task29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// Данные вводятся с консоли пользователем
+
+int[] array = new int[8];
+Console.Write("Введите 8 чисел: ");
+
+for (int i = 0; i < 8; ++i)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine(string.Join(", ", array));
