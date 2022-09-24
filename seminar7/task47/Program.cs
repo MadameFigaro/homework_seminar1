a3b8067b229fd4affde934c9624aa6206b061be8
Yulia Romanovich <cruciverba@yandex.ru>
2022-09-24 15:36:42 +0300

    realized task47

---
+++ seminar7/task47/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 47.Задайте двумерный массив размером m×n, заполненный 
+// случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] GetMatrix(int size1, int size2) 
+{
+  double[,] matrix = new double[size1, size2];
+  Random rand = new Random();
+  for (int i = 0; i < size1; i++)
+  {
+    for (int j = 0; j < size2; j++)
+    {
+      matrix[i, j] = Math.Round(rand.NextDouble() * 20 - 10, 1);
+    }
+  }
+  return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j]}\t");
+    }
+    Console.WriteLine();
+  }
+}
+
+Console.WriteLine("Введите количество строк матрицы: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов матрицы: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+double[,] matr = GetMatrix(number1, number2);
+Console.WriteLine("Случайная матрица из вещественных чисел:");
+PrintMatrix(matr);
